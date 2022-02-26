880a812de68413053348a8fc9183d569e7d64833
navidabdi <artsanaat@gmail.com>
2022-02-26 17:28:49 +0330

    every solution that comes to my mind for isPowerOfTwo

--- is-prime-problem/app.js
+++ is-prime-problem/app.js
@@ -1,6 +1,21 @@
+// Every solution that comes to my mind
 function isPowerOfTwo(number) {
-  return;
+  if (number < 1) return false;
+
+  let dividedNum = number;
+  while (dividedNum !== 1) {
+    if (dividedNum % 2 !== 0) return false;
+
+    dividedNum = dividedNum / 2;
+  }
+  return true;
 }
+
+console.log(isPowerOfTwo(5));
+console.log(isPowerOfTwo(8));
+console.log(isPowerOfTwo(2));
+console.log(isPowerOfTwo(20));
+console.log(isPowerOfTwo(1024));
 
 // Best Case:
 // Avgare Case:
