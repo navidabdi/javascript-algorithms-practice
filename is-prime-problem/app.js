a516e9111488a91165b049626642818c259c393d
navidabdi <artsanaat@gmail.com>
2022-02-26 13:15:12 +0330

    better solution for isPrime problem :)

--- is-prime-problem/app.js
+++ is-prime-problem/app.js
@@ -1,11 +1,19 @@
+// function isPrime(number) {
+//   const numArray = [];
+//   for (let i = 1; i < number + 1; i++) {
+//     if (number % i === 0) numArray.push(i);
+//   }
+//   if (numArray.length > 2) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
+
 function isPrime(number) {
-  const numArray = [];
-  for (let i = 1; i < number + 1; i++) {
-    if (number % i === 0) numArray.push(i);
+  for (let i = 2; i < number; i++) {
+    console.log(i);
+    if (number % i === 0) return false;
   }
-  if (numArray.length > 2) {
-    return false;
-  } else {
-    return true;
-  }
+  return true;
 }
