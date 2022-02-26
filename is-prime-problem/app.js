c9358f81af1c6a9abf23596e3caa396f666fd3dd
navidabdi <artsanaat@gmail.com>
2022-02-26 17:13:36 +0330

    share on github

--- is-prime-problem/app.js
+++ is-prime-problem/app.js
@@ -1,10 +1,7 @@
-function isPrime(number) {
-  for (let i = 2; i < Math.sqrt(number); i++) {
-    if (number % i === 0) return false;
-  }
-  return true;
+function isPowerOfTwo(number) {
+  return;
 }
 
-// Best Case: number = 1 OR 2 => O(1)
-// Avgare Case: number is diffrent so => O(n) => {Improved: O(sqrt(n))}
-// Worst Case:  number = 27,277 => O(n) => {Improved: O(sqrt(n))}
+// Best Case:
+// Avgare Case:
+// Worst Case:
