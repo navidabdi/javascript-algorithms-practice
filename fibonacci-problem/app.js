c52c77d363c45fa87be53408782c689537b46e3e
navidabdi <artsanaat@gmail.com>
2022-02-26 12:02:30 +0330

    change the Fibonacci Algorithims in a affective way

--- fibonacci-problem/app.js
+++ fibonacci-problem/app.js
@@ -1,11 +1,7 @@
 function fib(number) {
-  let firstArray = [1, 1];
-  let fibSum = 0;
-  if (number === 1 || number === 2) return 1;
-
-  for (let i = 0; i < number; i++) {
-    firstArray.push(firstArray[i] + firstArray[i + 1]);
-    fibSum = firstArray[i + 1];
+  let numbers = [1, 1];
+  for (let i = 2; i < number + 1; i++) {
+    numbers.push(numbers[i - 2] + numbers[i - 1]);
   }
-  return fibSum;
+  return numbers[number];
 }
