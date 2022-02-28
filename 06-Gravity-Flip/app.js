98dc32ecf8115c9de2982367bf4982e9a15ced69
navidabdi <artsanaat@gmail.com>
2022-02-28 10:46:35 +0330

    solve the Gravity Flip

---
+++ 06-Gravity-Flip/app.js
@@ -0,0 +1,64 @@
+/*
+Bob is bored during his physics lessons so he's built himself a toy box to help pass the time. The box is special because it has the ability to change gravity.
+
+There are some columns of toy cubes in the box arranged in a line. The i-th column contains a_i cubes. At first, the gravity in the box is pulling the cubes downwards. When Bob switches the gravity, it begins to pull all the cubes to a certain side of the box, d, which can be either 'L' or 'R' (left or right). Below is an example of what a box of cubes might look like before and after switching gravity.
+
+
++---+                                       +---+
+|   |                                       |   |
++---+                                       +---+
++---++---+     +---+              +---++---++---+
+|   ||   |     |   |   -->        |   ||   ||   |
++---++---+     +---+              +---++---++---+
++---++---++---++---+         +---++---++---++---+
+|   ||   ||   ||   |         |   ||   ||   ||   |
++---++---++---++---+         +---++---++---++---+
+
+Given the initial configuration of the cubes in the box, find out how many cubes are in each of the n columns after Bob switches the gravity.
+
+Examples:
+
+flip('R', [3, 2, 1, 2])     =>  [1, 2, 2, 3]
+flip('L', [1, 4, 5, 3, 5])  =>  [5, 5, 4, 3, 1]
+
+*/
+
+// Solution
+
+// const flip = (d, a) => {
+//   let newArray = [];
+//   let firstElement = a[0];
+//   for (let i = 0; i < a.length; i++) {
+//     if (d == 'R') {
+//       if (firstElement < a[i]) {
+//         firstElement = a[i];
+//         newArray.push(firstElement);
+//       }
+//     } else if (d == 'L') {
+//       if (firstElement > a[i]) {
+//         firstElement = a[i];
+//         newArray.push(firstElement);
+//       }
+//     } else {
+//       return newArray;
+//     }
+//   }
+//   return newArray;
+// };
+
+const flip = (d, a) => {
+  if (d == 'R')
+    return a.sort(function (a, b) {
+      return a - b;
+    });
+  if (d == 'L')
+    return a.sort(function (a, b) {
+      return b - a;
+    });
+  return;
+};
+
+// Test The Solution
+console.log(flip('R', [3, 2, 1, 2, 20]));
+console.log(flip('L', [1, 4, 5, 3, 5, 10]));
+console.log(flip('L', [86, 57, 5, 48, 45, 4, 34, 30, 1]));
