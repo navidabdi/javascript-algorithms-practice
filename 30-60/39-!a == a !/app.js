555db3cf8d82fd52e603ffd67beeffafb0cd3b47
navidabdi <artsanaat@gmail.com>
2022-03-10 09:13:54 +0330

    !a == a ?! is done

---
+++ 30-60/39-!a == a !/app.js
@@ -0,0 +1,22 @@
+/*
+
+You task to pass only this tests :
+
+a == false
+!a == false
+a == !a
+
+*/
+
+// My Solution 1
+const a = '0';
+
+// My Solution 2
+
+// Test the Solution
+console.log(); //
+
+// One of other Solutions
+const b = [];
+// One of other Solutions
+const c = new Boolean();
