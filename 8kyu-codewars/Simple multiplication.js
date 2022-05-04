00e79c0ab4bc49f4458e80062d413cbeddc3cc94
navidabdi <artsanaat@gmail.com>
2022-05-04 09:15:14 +0430

    Simple multiplication

---
+++ 8kyu-codewars/Simple multiplication.js
@@ -0,0 +1,23 @@
+/*
+
+Simple multiplication
+
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+*/
+
+// My Solution
+function simpleMultiplication(number) {
+  return number % 2 === 0 ? number * 8 : number * 9;
+}
+// One of other Solutions
+function simpleMultiplication(n) {
+  return n * (n % 2 ? 9 : 8);
+}
+// One of other Solutions
+
+// One of other Solutions
+
+// One of other Solutions
+
+// One of other Solutions
